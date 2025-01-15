2fd3710d99f7eee36eb0bc7d2e04e63dd1ff4d48
Amrit Sinha <amrit-sinha@outlook.com>
2025-01-16 02:04:38 +0530

    added refresh button

--- src/components/cricketers/page.tsx
+++ src/components/cricketers/page.tsx
@@ -3,11 +3,16 @@ import { columns } from "./columns";
 import { DataTable } from "./data-table";
 import { TPlayerType, TPlayer } from "@/apis/types";
 import getPlayers from "@/apis/get-players";
+import { Button } from "../ui/button";
+import { RefreshCw } from "lucide-react";
+import { Skeleton } from "../ui/skeleton";
 
 export default function CricketersList() {
   const [players, setPlayers] = useState<TPlayer[]>([]);
   const [error, setError] = useState<string | null>(null);
-  useEffect(() => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const fetchPlayers = () => {
+    setLoading(true);
     getPlayers()
       .then((players) => {
         const updatedPlayers = players.map((player) => {
@@ -17,16 +22,39 @@ export default function CricketersList() {
         });
         console.log(updatedPlayers);
         setPlayers(updatedPlayers);
+        setLoading(false);
       })
       .catch((error) => {
         setError(error.message);
+        setLoading(false);
       });
+  };
+
+  useEffect(() => {
+    fetchPlayers();
   }, []);
+
+  const handleRefresh = () => {
+    setPlayers([]);
+    fetchPlayers();
+  };
 
   return (
     <div className="container mx-auto py-10">
-      {!!players.length && <DataTable columns={columns} data={players} />}
-      {error && <p>{error}</p>}
+      <Button variant="outline" size="icon" onClick={handleRefresh}>
+        <RefreshCw />
+      </Button>
+      {loading ? (
+        <div className="flex items-center space-x-4">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[250px]" />
+            <Skeleton className="h-4 w-[200px]" />
+          </div>
+        </div>
+      ) : (
+        <DataTable columns={columns} data={players} />
+      )}
     </div>
   );
 }
